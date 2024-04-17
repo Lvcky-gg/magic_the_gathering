49b3bfd4bf32a7dcab60e087384862fbe2714398
lvcky-gg <matadorcrab@gmail.com>
2024-04-16 22:11:13 -0400

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,23 @@
 <script setup>
 
 import { useMagicStore } from '../stores/magic'
-import { onMounted } from 'vue';
+import { onMounted, toRaw } from 'vue';
 
 const magicStore = useMagicStore()
-onMounted(() => {
-  magicStore.fetchCards()
-  console.log(magicStore)
+onMounted(async () => {
+  await magicStore.fetchCards()
+  console.log(toRaw(magicStore.cards))
   
 })
 
 </script>
 
 <template>
-  <Card v-for="card in magicStore.cards">
+  <Card v-for="card in magicStore.cards" :key="card.name">
+    <template #title>
+      <h2>{{ card.name }}</h2>
+    </template>
+   <a href="card.imageUrl">{{ card }}</a> 
 
   </Card>
 
